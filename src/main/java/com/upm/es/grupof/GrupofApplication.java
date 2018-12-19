7641818ac70323192c6e7f6dbccd34fc1768a3b3
AlejandroBudy <atorresato@gmail.com>
2018-12-19 09:12:39 +0100

    Init repo

---
+++ src/main/java/com/upm/es/grupof/GrupofApplication.java
@@ -0,0 +1,14 @@
+package com.upm.es.grupof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupofApplication.class, args);
+	}
+
+}
+
