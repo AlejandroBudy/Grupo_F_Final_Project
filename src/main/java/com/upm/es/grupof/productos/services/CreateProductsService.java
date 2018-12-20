d1568be3bdc9a55906f113548db6f6bc02a3dd26
jlcastanon <jl.castanon@alumnos.upm.es>
2018-12-20 18:15:55 +0100

    Comment some logics

--- src/main/java/com/upm/es/grupof/productos/services/CreateProductsService.java
+++ src/main/java/com/upm/es/grupof/productos/services/CreateProductsService.java
@@ -7,4 +7,20 @@ import org.springframework.stereotype.Service;
 public class CreateProductsService {
 
 	// TODO implement logic
+
+    /*
+    Base logic
+        1. Prove usr does not exist
+            1.1 NOT EXIST
+                add ussr -> database
+            1.2 EXIST
+                throw error
+     */
+
+
+    public void createProduct(){
+
+    }
+
+
 }
