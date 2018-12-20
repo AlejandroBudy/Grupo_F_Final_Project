1b55a285cbc25a9d2abaf4a5743e4703b4e6cd86
AlejandroBudy <atorresato@gmail.com>
2018-12-20 18:24:40 +0100

    Properties changed,database initialzation and get product implemented

--- src/main/java/com/upm/es/grupof/productos/controller/ProductsController.java
+++ src/main/java/com/upm/es/grupof/productos/controller/ProductsController.java
@@ -1,12 +1,29 @@
 package com.upm.es.grupof.productos.controller;
 
+import com.upm.es.grupof.productos.entities.Product;
+import com.upm.es.grupof.productos.services.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "/products")
 public class ProductsController {
 
-	// TODO delete method
-	public  String dummyMethod(){
-		return "Hello world";
+
+	@Autowired
+	private ProductsService productsService;
+
+	@RequestMapping(value = "/{name}",
+			method = RequestMethod.GET,
+			consumes = "application/json",
+			produces = "application/json")
+	public Product getProduct(@PathVariable("name") String name){
+		return this.productsService.getProductByName(name);
 	}
+
+
 }
--- src/main/java/com/upm/es/grupof/productos/database/DataBaseLoader.java
+++ src/main/java/com/upm/es/grupof/productos/database/DataBaseLoader.java
@@ -1,10 +1,13 @@
 package com.upm.es.grupof.productos.database;
 
+import com.upm.es.grupof.productos.entities.Category;
+import com.upm.es.grupof.productos.entities.Product;
 import com.upm.es.grupof.productos.repository.ProductsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Component
 public class DataBaseLoader {
@@ -15,5 +18,23 @@ public class DataBaseLoader {
 	@PostConstruct
 	private void initDataBase(){
 
+		repository.save(new Product(Category.ROPA,"Jeans"));
+		repository.save(new Product(Category.ROPA,"T-shirt"));
+		repository.save(new Product(Category.ROPA,"Jersey"));
+		repository.save(new Product(Category.ROPA,"Coat"));
+
+		repository.save(new Product(Category.COMIDA,"Avocado"));
+		repository.save(new Product(Category.COMIDA,"Omelette"));
+		repository.save(new Product(Category.COMIDA,"Fruit"));
+		repository.save(new Product(Category.COMIDA,"Burger"));
+
+	}
+
+
+	public Product getProductByName(String name) throws Exception {
+		List<Product> productList = repository.findByName(name);
+		Product product = productList.iterator().next();
+		if(product == null) throw new Exception("Not Product Found");
+		return product;
 	}
 }
